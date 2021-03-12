35a030cb0e251f8ddc3a0f9e1f379f66aef5d0e4
Hamza Nadeem <hnb351@gmail.com>
2021-03-12 03:17:33 +0000

    adding char length program

---
+++ string_len.js
@@ -0,0 +1,4 @@
+var total_len = 280;
+var char = "hi there i am writing some letters";
+var remain_char=total_len - (char.length);
+console.log("you have written " + char.length + " characters and you have only " +remain_char+ " left");
