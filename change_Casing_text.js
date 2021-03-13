7b3ed24bbefdb70abcdddd0a0700e2032ddd8cc4
Hamza Nadeem <hnb351@gmail.com>
2021-03-13 03:31:49 +0000

    name changing text alert using js

---
+++ change_Casing_text.js
@@ -0,0 +1,5 @@
+var char = prompt("Compose your name");
+// var remain_char=total_len - (char.length);
+var sliced_char=(char.slice(0,1))
+
+alert("Hi "+sliced_char.toUpperCase()+char.slice(1,length.char));
