a7e54d32190054b0039e8dd376ab78af1e57c86d
Hamza Nadeem <hnb351@gmail.com>
2021-03-16 02:53:52 +0000

    minor change

--- functions.js
+++ functions.js
@@ -1,10 +1,7 @@
 function getMilk(bottleofmilks,milkMoney)
 {
   console.log("leave house");
-  console.log("leave house");
-  console.log("leave house");
-  console.log("leave house");
-  console.log("leave house");
+
   console.log("got " +bottleofmilks+" bottles of milk worth $"+milkMoney);
 }
 getMilk(3,3);
