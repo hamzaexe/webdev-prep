dc629972c956e1a4764865ed9f6cd57bc04b95bf
Hamza Nadeem <hnb351@gmail.com>
2021-03-16 02:50:07 +0000

    add function script

---
+++ functions.js
@@ -0,0 +1,10 @@
+function getMilk(bottleofmilks,milkMoney)
+{
+  console.log("leave house");
+  console.log("leave house");
+  console.log("leave house");
+  console.log("leave house");
+  console.log("leave house");
+  console.log("got " +bottleofmilks+" bottles of milk worth $"+milkMoney);
+}
+getMilk(3,3);
