da68bef65489d4c157f6082fe612d932d53e9038
Hamza Nadeem <hnb351@gmail.com>
2021-03-17 08:36:49 +0000

    minor fixes

--- bmi-calculator.js
+++ bmi-calculator.js
@@ -2,19 +2,21 @@ function calculatBmi(weight, height)
 {
   bmi=weight/(height*height);
   bmi=Math.floor(bmi);
+  var result;
   if (bmi<18.5)
   {
-    return "your bmi is "+bmi+" you are underweight";
+    result="your bmi is "+bmi+" you are underweight";
   }
   else if(bmi>=18.5 && bmi<=24.9)
   {
-  return "your bmi is "+bmi+" so you have a normal weight";
+  result="your bmi is "+bmi+" so you have a normal weight";
   }
   else if(bmi>24.9)
   {
-    return "your bmi is "+bmi+" so you are over-weight";
+    result="your bmi is "+bmi+" so you are over-weight";
   }
+  return result;
 }
-var val=calculatBmi(85,1.8);
-console.log(val);
+console.log(calculatBmi(85,1.8));
+
 
