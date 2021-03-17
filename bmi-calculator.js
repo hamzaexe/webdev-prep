f5cbb5a9448e266664c155006821e26e138e553a
Hamza Nadeem <hnb351@gmail.com>
2021-03-17 08:31:48 +0000

    function that outputs (returns) a different message depending on the BMI.

--- bmi-calculator.js
+++ bmi-calculator.js
@@ -1,8 +1,20 @@
 function calculatBmi(weight, height)
 {
   bmi=weight/(height*height);
-  return bmi
+  bmi=Math.floor(bmi);
+  if (bmi<18.5)
+  {
+    return "your bmi is "+bmi+" you are underweight";
+  }
+  if(bmi>=18.5 && bmi<=24.9)
+  {
+  return "your bmi is "+bmi+" so you have a normal weight";
+  }
+  if(bmi>24.9)
+  {
+    return "your bmi is "+bmi+" so you are over-weight";
+  }
 }
-var value=calculatBmi(75,1.8);
+var val=calculatBmi(75,2);
+console.log(val);
 
-console.log(Math.round(value));
