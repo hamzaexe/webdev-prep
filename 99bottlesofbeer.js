14dd3b506daea2326d9c162f7537ffae9cbb881b
Hamza Nadeem <hnb351@gmail.com>
2021-03-19 11:13:18 +0000

    99 bottles of bear challenge

---
+++ 99bottlesofbeer.js
@@ -0,0 +1,13 @@
+var beer=99;
+var wall;
+function bottle()
+{
+while(beer>=1)
+{
+  wall=beer-1;
+console.log(beer+" bottles of beer on the wall "+beer+ " bottles of beer Take one down and pass it around "+wall+ " bottles of beer on the wall.");
+beer-=1;
+}
+console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.")
+}
+bottle()
